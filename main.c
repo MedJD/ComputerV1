8532c2daf28d1dc0cad5ac8c87214a0e00208409
Mohammed El jadi <mel-jadi@e1r7p6.1337.ma>
2020-11-02 20:47:12 +0100

    first

--- main.c
+++ main.c
@@ -3,7 +3,15 @@
 #include <stdlib.h>
 #include <ctype.h>
 
-char *delete_spaces(char *s)
+typedef struct  s_eq
+{
+    float   a;
+    float   b;
+    float   c;
+    char    *var;
+}               t_eq;
+
+char    *delete_spaces(char *s)
 {
     char    *res;
     int     size;
@@ -17,7 +25,7 @@ char *delete_spaces(char *s)
         test = 0;
         if (s[i] != ' ' && s[i] != '\v' && s[i] != '\t' && (test = 1) == 1)
             size++;
-        if (!isalnum(s[i]) && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '^' && test)
+        if (!isalnum(s[i]) && s[i] != '+' && s[i] != '-' && s[i] != '*' && s[i] != '^' && s[i] != '=' && s[i] != '.' && test)
         {
             printf("equation fausse ou insupportable\n");
             exit(0);
@@ -42,15 +50,54 @@ char *delete_spaces(char *s)
     return(res);
 }
 
-int main(int ac, char **av)
+// int     check_form(char *s)
+// {
+//     int     i;
+
+//     i = -1;
+//     if (strlen(s) < 3)
+//     {
+//         printf("equation fausse ou insupportable\n");
+//         exit(0);
+//     }
+//     while (s[++i])
+//     {
+        
+//     }
+    
+// }
+
+// int     str_to_eq(char*s, t_eq *eq)
+// {
+//     int     debut;
+//     int     fin;
+//     int     i;
+
+//     i = -1;
+//     while (s[++i])
+//     {
+//         if (isdigit(s[i]))
+//         {
+
+//         }
+//     }
+
+// }
+
+int     main(int ac, char **av)
 {
-    char *eq;
+    char    *str_eq;
+    t_eq    eq;
 
     if (ac != 2)
     {
         printf("usage: kada kada\n");
         return(0);
     }
-    eq = delete_spaces(av[1]);
+    str_eq = delete_spaces(av[1]);
+    eq.a = 0;
+    eq.b = 0;
+    eq.c = 0;
+
     return(0);
 }
